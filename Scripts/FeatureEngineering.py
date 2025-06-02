93519d853aef82d320a58b4e03cf4b61cacbb9dc
stmaster <maximilian.sterz@student.hs-anhalt.de>
2025-06-02 11:06:05 +0200

    looking for weekdays

--- Scripts/FeatureEngineering.py
+++ Scripts/FeatureEngineering.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-combined_data = pd.read_csv('combined_gold_economic_factors.csv')
+combined_data = pd.read_csv('../Data/combined_gold_economic_factors.csv')
 
 # Umbenennen der Spalte von "Goldpreis_am_Tag" in "Goldpreis", falls noch nicht geschehen
 if 'Goldpreis_am_Tag' in combined_data.columns:
@@ -18,5 +18,11 @@ combined_data['Date'] = pd.to_datetime(combined_data['Date'])
 combined_data['Wochentag'] = combined_data['Date'].dt.day_name()
 
 print(combined_data.head(20))
+print(combined_data.groupby(['Wochentag']).count())
 
-combined_data.to_csv('combined_gold_economic_factors_withweekdays.csv', index=False)
+gold=pd.read_csv('../Data/Goldpreis.csv')
+
+gold['Date'] = pd.to_datetime(combined_data['Date'])
+gold['Wochentag'] = combined_data['Date'].dt.day_name()
+print(gold.groupby(['Wochentag']).count())
+#combined_data.to_csv('combined_gold_economic_factors_withweekdays.csv', index=False)
