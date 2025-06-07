584bd6fb69f29ef54e461ecd7a2207892ae5fdd7
Sterz, Maximilian <maximilian.sterz@student.hs-anhalt.de>
2025-06-07 12:39:00 +0000

    Update file FeatureEngineering.py

--- Scripts/FeatureEngineering.py
+++ Scripts/FeatureEngineering.py
@@ -1,22 +1,59 @@
 import pandas as pd
 
-combined_data = pd.read_csv('combined_gold_economic_factors.csv')
+gold_data = pd.read_csv('Goldpreis.csv', index_col=0)
+gold_data.reset_index(inplace=True)
+gold_data.columns = ['Date', 'Goldpreis_am_Tag']
 
-# Umbenennen der Spalte von "Goldpreis_am_Tag" in "Goldpreis", falls noch nicht geschehen
-if 'Goldpreis_am_Tag' in combined_data.columns:
-    combined_data.rename(columns={'Goldpreis_am_Tag': 'Goldpreis'}, inplace=True)
+if 'Goldpreis_am_Tag' in gold_data.columns:
+    gold_data.rename(columns={'Goldpreis_am_Tag': 'Goldpreis'}, inplace=True)
 
-# Überprüfe auf fehlende Daten
-missing_data = combined_data.isnull().sum()
+#Auf Null Werte prüfen
+missing_data = gold_data.isnull().sum()
 print("Anzahl der fehlenden Werte in jeder Spalte:")
 print(missing_data)
 
-# Test ob "Date"-Spalte im Datetime-Format
-combined_data['Date'] = pd.to_datetime(combined_data['Date'])
 
-# neue Spalte die den Wochentag enthält
-combined_data['Wochentag'] = combined_data['Date'].dt.day_name()
+gold_data['Date'] = pd.to_datetime(gold_data['Date'])
 
-print(combined_data.head(20))
+# Wochentage/Monat/Quartal aus Datum extrahieren
+gold_data['Tag'] = gold_data['Date'].dt.day
+gold_data['Monat'] = gold_data['Date'].dt.month
+gold_data['Jahr'] = gold_data['Date'].dt.year
+gold_data['Wochentag'] = gold_data['Date'].dt.day_name()
+gold_data['Quartal'] = gold_data['Date'].dt.quarter
 
-combined_data.to_csv('combined_gold_economic_factors_withweekdays.csv', index=False)
+
+
+# One-Hot-Encoding für Wochentage
+weekday_dummies = pd.get_dummies(gold_data['Wochentag'], prefix='Weekday')
+gold_data = pd.concat([gold_data, weekday_dummies], axis=1)
+
+#day_dummies = pd.get_dummies(gold_data['Tag'], prefix='Tag')
+#gold_data = pd.concat([gold_data, day_dummies], axis=1)
+
+#monat_dummies = pd.get_dummies(gold_data['Monat'], prefix='Monat')
+#gold_data = pd.concat([gold_data, monat_dummies], axis=1)
+
+quartalsdummies = pd.get_dummies(gold_data['Quartal'], prefix='Quartal')
+gold_data = pd.concat([gold_data, quartalsdummies], axis=1)
+
+#jahr_dummies = pd.get_dummies(gold_data['Jahr'], prefix='Jahr')
+#gold_data = pd.concat([gold_data, jahr_dummies], axis=1)
+
+
+gold_data.sort_values(by='Date', ascending=False, inplace=True)
+gold_data['Preisänderung'] = gold_data['Goldpreis'].pct_change(periods=-1) * 100
+
+
+gold_data = gold_data[gold_data['Date'] >= '2021-01-01']
+gold_data.sort_values(by='Date', inplace=True, ascending=False)
+
+
+
+#gold_data.sort_values(by='Date', ascending=True, inplace=True)
+
+
+gold_data = gold_data[gold_data['Date'].dt.weekday < 5]
+encoded_gold_data=gold_data.drop(columns=['Wochentag','Quartal'])
+print(gold_data.head(20))
+encoded_gold_data.to_csv('gold_with_features_encoded.csv', index=False)
