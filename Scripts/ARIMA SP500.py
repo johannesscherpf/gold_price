2a99a75364421e550eca5c8af3c6e75e2da1d5f7
hannes <johannespetersamuel.scherpf@student.hs-anhalt.de>
2025-06-18 15:01:22 +0200

    Vorhersagen SP500

--- Scripts/ARIMA SP500.py
+++ Scripts/ARIMA SP500.py
@@ -20,25 +20,25 @@ train_size = int(0.8 * len(df_SP500))
 train, test = df_SP500[:train_size], df_SP500[train_size:]
 
 # ARIMA Modell fitten
-arima_model_SP = ARIMA(train, order=(3, 2, 2))
+arima_model_SP = (ARIMA(train, order=(3, 2, 3)))
 arima_model_SP_fit = arima_model_SP.fit()
 
 # Vorhersage für die Testdaten
 arima_forecast_SP = arima_model_SP_fit.forecast(steps=len(test))
 
 # MSE berechnen
-mse_Goldpreis = mean_squared_error(test, arima_forecast_SP)
+mse_SP500 = mean_squared_error(test, arima_forecast_SP)
 
 # Ergebnisse anzeigen
-print(f'MSE für SP500: {mse_Goldpreis}')
+print(f'MSE für SP500: {mse_SP500}')
 
 # Plot für die Vorhersage des Goldpreises
 plt.figure(figsize=(14, 7))
-plt.plot(train, color='darkblue', label='Trainingsdaten Goldpreis')
-plt.plot(test.index, test, color='lightblue', label='Testdaten Goldpreis')
+plt.plot(train, color='darkblue', label='Trainingsdaten')
+plt.plot(test.index, test, color='lightblue', label='Testdaten')
 plt.plot(test.index, arima_forecast_SP, color='darkblue', linestyle='--', label='Vorhersage Goldpreis')
 plt.xlabel('Date')
 plt.ylabel('Value')
-plt.title('ARIMA Vorhersage für den Goldpreis')
+plt.title('ARIMA Vorhersage SP500')
 plt.legend()
 plt.show()
